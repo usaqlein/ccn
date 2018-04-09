58fc87a64656b1f175d4fc05bd510fa70cc4fd51
usaqlain01 <usaqlain01@gmail.com>
2018-04-09 01:11:42 -0500

    templating and grid sizes part one

--- web/themes/gavias_financial/includes/override.php
+++ web/themes/gavias_financial/includes/override.php
@@ -1,6 +1,14 @@
 <?php
 function gavias_financial_preprocess_views_view_grid(&$variables) {
+    /** @var Drupal\views\ViewExecutable $view */
    $view = $variables['view'];
+   if($view) {
+     /** @var Drupal\views\Entity\View $viewStorage */
+     $viewStorage = $view->storage;
+     if($viewStorage) {
+       $variables['gridViewName'] = 'grid-'.$viewStorage->id();
+     }
+   }
    $rows = $variables['rows'];
    $style = $view->style_plugin;
    $options = $style->options;
