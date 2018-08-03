50512065316d1a2948f47269df24230a67e1b753
usaqlain01 <usaqlain01@gmail.com>
2018-08-03 14:17:30 -0500

    Added contact us forms of 3 kinds

--- web/themes/gavias_financial/includes/contact.php
+++ web/themes/gavias_financial/includes/contact.php
@@ -1,6 +1,6 @@
 <?php
 function gavias_financial_form_contact_message_feedback_form_alter(&$form, \Drupal\Core\Form\FormStateInterface $form_state, $form_id) {
-  
+  //contact_form=feedback
   // Name
   $form['name']['#weight'] = -1;
   $form['name']['#prefix'] = '<div class="contact-feedback"><div class="form-group">';
@@ -41,12 +41,13 @@ function gavias_financial_form_contact_message_feedback_form_alter(&$form, \Drup
   $form['actions']['#suffix'] = '</div></div>';
   $form['actions']['submit']['#attributes']['class'][] = 'btn';
   $form['actions']['submit']['#attributes']['class'][] = 'btn-theme-submit';
-  
+  if (isset($form['actions']['preview'])) {
+    unset($form['actions']['preview']);
+  }
 }
 
 function gavias_financial_form_contact_message_request_call_back_form_alter(&$form, \Drupal\Core\Form\FormStateInterface $form_state, $form_id) {
-  
-  
+  //contact_form=request_call_back
   unset($form['mail']);
   unset($form['message']);
   //print_r($form['#attributes']);
@@ -94,4 +95,137 @@ function gavias_financial_form_contact_message_request_call_back_form_alter(&$fo
   $form['html_after']['#weight'] = 99;
   $form['html_after']['#prefix'] = '';
   $form['html_after']['#suffix'] = '</div></div>';
+  $form['actions']['submit']['#value'] = t('Report');
+  if (isset($form['actions']['preview'])) {
+    unset($form['actions']['preview']);
+  }
+}
+
+function gavias_financial_form_contact_message_recommend_content_form_alter(&$form, \Drupal\Core\Form\FormStateInterface $form_state, $form_id) {
+  //contact_form=recommend_content
+
+  $form['#enctype'] = 'multipart/form-data';
+
+  // Content Type
+  $form['field_recommend_content_type']['#weight'] = -90;
+  $form['field_recommend_content_type']['#prefix'] = '<div class="contact-feedback"><div class="form-group">';
+  $form['field_recommend_content_type']['#suffix'] = '</div>';
+
+  // Name
+  $form['name']['#weight'] = -1;
+  $form['name']['#prefix'] = '<div class="form-group">';
+  $form['name']['#suffix'] = '</div>';
+  $form['name']['#attributes']['placeholder'][] = $form['name']['#title'].'*';
+  $form['name']['#attributes']['class'][] = 'form-control';
+  unset($form['name']['#title']);
+
+  // Mail
+  $form['mail']['#weight'] = -2;
+  $form['mail']['#prefix'] = '<div class="form-group">';
+  $form['mail']['#suffix'] = '</div>';
+  $form['mail']['#attributes']['placeholder'][] = $form['mail']['#title'].'*';
+  $form['mail']['#attributes']['class'][] = 'form-control';
+  unset($form['mail']['#title']);
+
+  // Message
+  $form['message']['#weight'] = -5;
+  $form['message']['widget'][0]['value']['#attributes']['class'][] = 'form-control';
+  $form['message']['widget'][0]['value']['#attributes']['placeholder'][] = $form['message']['widget'][0]['#title'].'*';
+  $form['message']['widget'][0]['#title'] = '';
+  unset($form['message']['widget'][0]['value']['#title']);
+  $form['message']['widget'][0]['#prefix'] = '<div class="clearfix"><div class="form-group">';
+  $form['message']['widget'][0]['#suffix'] = '</div></div>';
+
+  // Submit
+  $form['actions']['#weight'] = 99;
+  $form['actions']['#prefix'] = '<div class="clearfix">';
+  $form['actions']['#suffix'] = '</div></div>';
+  $form['actions']['submit']['#attributes']['class'][] = 'btn';
+  $form['actions']['submit']['#attributes']['class'][] = 'btn-theme-submit';
+  if (isset($form['actions']['preview'])) {
+    unset($form['actions']['preview']);
+  }
+}
+
+function gavias_financial_form_contact_message_contact_us_form_alter(&$form, \Drupal\Core\Form\FormStateInterface $form_state, $form_id) {
+  //contact_form=contact_us
+  $form['#enctype'] = 'multipart/form-data';
+
+  // Content Type
+  $form['field_contact_reason']['#weight'] = -90;
+  $form['field_contact_reason']['#prefix'] = '<div class="contact-feedback"><div class="form-group">';
+  $form['field_contact_reason']['#suffix'] = '</div>';
+
+  // Name
+  $form['name']['#weight'] = -1;
+  $form['name']['#prefix'] = '<div class="form-group">';
+  $form['name']['#suffix'] = '</div>';
+  $form['name']['#attributes']['placeholder'][] = $form['name']['#title'].'*';
+  $form['name']['#attributes']['class'][] = 'form-control';
+  unset($form['name']['#title']);
+
+  // Mail
+  $form['mail']['#weight'] = -2;
+  $form['mail']['#prefix'] = '<div class="form-group">';
+  $form['mail']['#suffix'] = '</div>';
+  $form['mail']['#attributes']['placeholder'][] = $form['mail']['#title'].'*';
+  $form['mail']['#attributes']['class'][] = 'form-control';
+  unset($form['mail']['#title']);
+
+  // Message
+  $form['message']['#weight'] = -5;
+  $form['message']['widget'][0]['value']['#attributes']['class'][] = 'form-control';
+  $form['message']['widget'][0]['value']['#attributes']['placeholder'][] = $form['message']['widget'][0]['#title'].'*';
+  $form['message']['widget'][0]['#title'] = '';
+  unset($form['message']['widget'][0]['value']['#title']);
+  $form['message']['widget'][0]['#prefix'] = '<div class="clearfix"><div class="form-group">';
+  $form['message']['widget'][0]['#suffix'] = '</div></div>';
+
+  // Submit
+  $form['actions']['#weight'] = 99;
+  $form['actions']['#prefix'] = '<div class="clearfix">';
+  $form['actions']['#suffix'] = '</div></div>';
+  $form['actions']['submit']['#attributes']['class'][] = 'btn';
+  $form['actions']['submit']['#attributes']['class'][] = 'btn-theme-submit';
+  if (isset($form['actions']['preview'])) {
+    unset($form['actions']['preview']);
+  }
+}
+
+function gavias_financial_form_contact_message_subscribe_form_alter(&$form, \Drupal\Core\Form\FormStateInterface $form_state, $form_id) {
+  //contact_form=subscribe
+  // Name
+  $form['name']['#weight'] = -1;
+  $form['name']['#prefix'] = '<div class="contact-feedback"><div class="form-group">';
+  $form['name']['#suffix'] = '</div>';
+  $form['name']['#attributes']['placeholder'][] = $form['name']['#title'].'*';
+  $form['name']['#attributes']['class'][] = 'form-control';
+  unset($form['name']['#title']);
+
+  // SITE UPDATES | field_site_updates
+  $form['field_site_updates']['#prefix'] = '<div class="pull-left margin-right-20"><div class="form-group">';
+  $form['field_site_updates']['#suffix'] = '</div></div>';
+
+  // CRYPTO NEWS & EVENTS | field_crypto_news_events
+  $form['field_crypto_news_events']['#prefix'] = '<div class="pull-left margin-right-20"><div class="form-group">';
+  $form['field_crypto_news_events']['#suffix'] = '</div></div>';
+
+  // Mail
+  $form['mail']['#weight'] = -2;
+  $form['mail']['#prefix'] = '<div class="form-group">';
+  $form['mail']['#suffix'] = '</div>';
+  $form['mail']['#attributes']['placeholder'][] = $form['mail']['#title'].'*';
+  $form['mail']['#attributes']['class'][] = 'form-control';
+  unset($form['mail']['#title']);
+
+  // Submit
+  $form['actions']['#weight'] = 99;
+  $form['actions']['#prefix'] = '<div class="clearfix pull-left">';
+  $form['actions']['#suffix'] = '</div></div>';
+  $form['actions']['submit']['#attributes']['class'][] = 'btn';
+  $form['actions']['submit']['#attributes']['class'][] = 'btn-theme-submit';
+  $form['actions']['submit']['#value'] = t('Subscribe');
+  if (isset($form['actions']['preview'])) {
+    unset($form['actions']['preview']);
+  }
 }
